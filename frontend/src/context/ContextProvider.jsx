4a9e540153b39dcf291342ab33ea569480e8f2a8
dorian-sz <dorian.szatmari@gmail.com>
2024-02-07 12:09:06 +0100

    Add auth context

---
+++ frontend/src/context/ContextProvider.jsx
@@ -0,0 +1,34 @@
+import { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext({
+	currentUser: null,
+	token: null,
+	isAdmin: null,
+	setUser: () => {},
+	setToken: () => {},
+	setIsAdmin: () => {},
+});
+
+export const ContextProvider = ({ children }) => {
+	const [user, setUser] = useState({});
+	const [isAdmin, setIsAdmin] = useState(false);
+	const [token, _setToken] = useState(localStorage.getItem('ACCES_TOKEN'));
+
+	const setToken = (token) => {
+		_setToken(token);
+		if (token) {
+			localStorage.setItem('ACCESS_TOKEN', token);
+		} else {
+			localStorage.removeItem('ACCES_TOKEN');
+		}
+	};
+
+	return (
+		<StateContext.Provider
+			value={{ user, token, isAdmin, setIsAdmin, setUser, setToken }}>
+			{children}
+		</StateContext.Provider>
+	);
+};
+
+export const useStateContext = () => useContext(StateContext);

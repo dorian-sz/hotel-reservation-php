16492fa82dfde0fb1a5a4ab0cfadedd6f4866b69
dorian-sz <dorian.szatmari@gmail.com>
2024-02-08 12:27:10 +0100

    Add signup page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,3 +1,86 @@
+import { useRef, useState } from 'react';
+import Form from '../components/Form/Form';
+import axiosClient from '../axios-client';
+import { useStateContext } from '../context/ContextProvider';
+
 export default function Signup() {
-	return <div>Signup</div>;
+	const fNameRef = useRef();
+	const lNameRef = useRef();
+	const emailRef = useRef();
+	const passwordRef = useRef();
+	const passwordConfirmationRef = useRef();
+
+	const [isLoading, setIsLoading] = useState(false);
+
+	const { setUser, setToken, setIsAdmin } = useStateContext();
+
+	const fields = [
+		{
+			label: 'Email address',
+			id: 'email',
+			type: 'email',
+			ref: emailRef,
+		},
+		{
+			label: 'First name',
+			id: 'fName',
+			type: 'text',
+			ref: fNameRef,
+		},
+		{
+			label: 'Last name',
+			id: 'lName',
+			type: 'text',
+			ref: lNameRef,
+		},
+		{
+			label: 'Password',
+			id: 'password',
+			type: 'password',
+			ref: passwordRef,
+		},
+		{
+			label: 'Confirm password',
+			id: 'confirmPassword',
+			type: 'password',
+			ref: passwordConfirmationRef,
+		},
+	];
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		setIsLoading(true);
+		const payload = {
+			email: emailRef.current.value,
+			first_Name: fNameRef.current.value,
+			last_Name: lNameRef.current.value,
+			password: passwordRef.current.value,
+			password_confirmation: passwordConfirmationRef.current.value,
+		};
+		axiosClient
+			.post('/signup', payload)
+			.then(({ data }) => {
+				setUser(data.user);
+				setIsAdmin(data.isAdmin);
+				setToken(data.token);
+			})
+			.catch((err) => {
+				const response = err.response;
+				if (response && response.status === 422) {
+					console.log(response.data.errors);
+				}
+			});
+	};
+
+	return (
+		<div className='flex w-full justify-center mt-20 '>
+			<Form
+				title={'Create a new account'}
+				fields={fields}
+				buttonLabel={'Register'}
+				onSubmit={onSubmit}
+				isLoading={isLoading}
+			/>
+		</div>
+	);
 }

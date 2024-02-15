1ce4d576e343ce8630037efc984daff8ecb42d0a
dorian-sz <dorian.szatmari@gmail.com>
2024-02-16 00:10:47 +0100

    Add reservation relation to user model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'first_name',
         'last_name',
         'email',
+        'reservations',
         'password',
     ];
 
@@ -43,4 +44,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
 }

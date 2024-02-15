cd5e38d88ce4b0eda9298242a1f784c977ba5451
dorian-sz <dorian.szatmari@gmail.com>
2024-02-16 00:21:43 +0100

    Add reservation resource class

---
+++ backend/app/Http/Resources/ReservationResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReservationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'total_cost' => $this->total_cost,
+            'rooms' => $this->rooms
+        ];
+    }
+}

af808ffaf3ea1b99b0389233281ec8508bd95b72
dorian-sz <dorian.szatmari@gmail.com>
2024-02-26 21:50:38 +0100

    Fix typo in getting access token

--- frontend/src/context/ContextProvider.jsx
+++ frontend/src/context/ContextProvider.jsx
@@ -12,14 +12,14 @@ const StateContext = createContext({
 export const ContextProvider = ({ children }) => {
 	const [user, setUser] = useState({});
 	const [isAdmin, setIsAdmin] = useState(false);
-	const [token, _setToken] = useState(localStorage.getItem('ACCES_TOKEN'));
+	const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
 
 	const setToken = (token) => {
 		_setToken(token);
 		if (token) {
 			localStorage.setItem('ACCESS_TOKEN', token);
 		} else {
-			localStorage.removeItem('ACCES_TOKEN');
+			localStorage.removeItem('ACCESS_TOKEN');
 		}
 	};
 

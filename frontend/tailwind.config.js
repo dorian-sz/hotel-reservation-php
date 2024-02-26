7ee7a2dc81d417cc32ca8fc3d789d16c1b0170c4
dorian-sz <dorian.szatmari@gmail.com>
2024-02-26 21:53:20 +0100

    Add min height and width config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
+		minWidth: {
+			'1/5': '20%',
+			'1/4': '25%',
+			'2/5': '40%',
+		},
+		minHeight: {
+			'1/5': '20%',
+			'1/4': '25%',
+			'2/5': '40%',
+		},
 		extend: {},
 	},
 	plugins: [],

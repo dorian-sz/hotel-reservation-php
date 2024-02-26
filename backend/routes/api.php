4270d09b5dab526b8c3d1cc17841e44c3e023ff2
dorian-sz <dorian.szatmari@gmail.com>
2024-02-26 21:53:31 +0100

    Move routes to auth middleware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -20,17 +20,22 @@ Route::middleware('auth:sanctum')->group(function (){
         return $request->user();
     });
     Route::post('/logout', [AuthController::class, 'logout']);
+
+
+    Route::apiResource('/users', \App\Http\Controllers\UserController::class);
+    Route::apiResource('/rooms', \App\Http\Controllers\RoomController::class);
+    Route::apiResource('/reservations', \App\Http\Controllers\ReservationController::class);
+    Route::apiResource('/sizes', \App\Http\Controllers\SizeController::class);
+
+    Route::post('/reservations/total-cost', [\App\Http\Controllers\ReservationController::class, 'totalCost']);
+
+    Route::get('/room-details/available', [RoomDetailsController::class, 'availableRooms']);
+    Route::get('/room-details/cost/{room}', [RoomDetailsController::class, 'getRoomCost']);
+    Route::get('/room-details/size/{room}', [RoomDetailsController::class, 'getRoomSize']);
 });
 
 
 Route::post('/signup', [AuthController::class, 'signup']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::apiResource('/users', \App\Http\Controllers\UserController::class);
-Route::apiResource('/rooms', \App\Http\Controllers\RoomController::class);
-Route::apiResource('/reservations', \App\Http\Controllers\ReservationController::class);
-Route::apiResource('/sizes', \App\Http\Controllers\SizeController::class);
 
-Route::get('/room-details/available', [RoomDetailsController::class, 'availableRooms']);
-Route::get('/room-details/cost/{room}', [RoomDetailsController::class, 'getRoomCost']);
-Route::get('/room-details/size/{room}', [RoomDetailsController::class, 'getRoomSize']);

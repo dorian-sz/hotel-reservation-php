c9d8b853db1466cc370752970e6737eb5ae6e7cc
dorian-sz <dorian.szatmari@gmail.com>
2024-02-27 20:58:45 +0100

    Add loading to button

--- frontend/src/components/PendingReservation/Button.jsx
+++ frontend/src/components/PendingReservation/Button.jsx
@@ -1,13 +1,15 @@
-import { Link } from 'react-router-dom';
 import { twMerge } from 'tailwind-merge';
 
-export default function Button() {
+export default function Button({ onClick, isLoading }) {
 	return (
 		<button
+			disabled={isLoading}
 			className={twMerge(
-				'text-center font-lg text-white font-bold w-full rounded-lg p-2 px-6 bg-red-600 hover:bg-red-800'
-			)}>
-			<Link to={'/make-reservation'}>Finalize Reservation</Link>
+				'text-center font-lg text-white font-bold w-full rounded-lg p-2 px-6 bg-red-600 hover:bg-red-800',
+				isLoading ? 'bg-red-800' : 'bg-red-600'
+			)}
+			onClick={() => onClick()}>
+			Finalize Reservation
 		</button>
 	);
 }
